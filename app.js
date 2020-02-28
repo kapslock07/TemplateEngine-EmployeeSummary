f27fe1ebde2073c90c752fbb62fec113eb56f93e
Ryan Kaplan <ryankaplan7@gmail.com>
2020-02-28 14:37:48 -0500

    inital code with classes and a few Qs

--- app.js
+++ app.js
@@ -4,30 +4,62 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
+
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
+
 const render = require("./lib/htmlRenderer");
-​
-​
+
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-​
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the name of your team manager?",
+            name: "managerName"
+        },
+        {
+            type: "input",
+            message: "What is the manager's ID number?",
+            name: "managerId"
+        },
+        {
+            type: "input",
+            message: "What is the manager's email?",
+            name: "managerEmail"
+        },
+        {
+            type: "number",
+            message: "How many engineers will there be?",
+            name: "numEngineers"
+        },
+        {
+            type: "number",
+            message: "How many interns will there be?",
+            name: "numInterns"
+        }
+    ])
+    .then(answers => {
+        // Use user feedback for... whatever!!
+    });
+
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
-​
+
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-​
+
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
-​
+
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
 // for further information. Be sure to test out each class and verify it generates an 
